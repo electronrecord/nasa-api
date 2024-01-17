cf13435b6ec23899d0b0eb3d26e0103653bfddd0
Mac <electronrecord@gmail.com>
2024-01-17 09:35:23 +0200

    removed Auth token from basecode

--- src/store/axios.js
+++ src/store/axios.js
@@ -10,8 +10,7 @@ const instance = axios.create({
 // Add an interceptor to include the Bearer token in all requests
 instance.interceptors.request.use(config => {
   const authToken = process.env.AUTH_BEARER; // Replace with your authentication token
-  // config.headers.Authorization = `Bearer ${authToken}`;
-  config.headers.Authorization = `eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZWNoUG9ydCIsImV4cCI6MTcwNTQxNjkyNywibmJmIjoxNzA1MzMwNTI3LCJTRVNTSU9OX0lEIjoiWm5xa0JjUGs2bzExMGRtWWZESDkwZVI4bWhtRUpRY1I1Q1VEIiwiRklOR0VSUFJJTlRfSEFTSCI6IjM0REQ1MDZBNjhDREZBQTZFQjgzNEY4NTcyNkExMzJBNzU2RTU1RDEzNkI4NUNENTJDNTkyMjg1RjhDMTI5MTkifQ.8w44Gk8_PfrNgSeRGd25tPfpl2UovcKj2gT9S6FP97k`;
+  config.headers.Authorization = `Bearer ${authToken}`;
   return config;
 }, error => {
   return Promise.reject(error);
