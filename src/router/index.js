20a015bbe7d74129e9ccbcbfe8bd0c856437e7a0
Mac <electronrecord@gmail.com>
2024-01-17 10:00:23 +0200

    router scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,19 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // If the route has a hash, scroll to the element with that id
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    // If the route has a saved position, use it
+    if (savedPosition) {
+      return savedPosition;
+    }
+    // Otherwise, scroll to the top
+    return { x: 0, y: 0 };
+  },
 })
 
 export default router
